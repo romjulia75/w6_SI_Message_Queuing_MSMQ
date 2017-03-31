8f8130c6ac40925b96fc07ae5f3f758f67380cc7
Roman Mashkovskyy <romjulia75@gmail.com>
2017-03-31 23:38:28 +0200

    no message

---
+++ Message_Queuing_MSMQ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message_Queuing_MSMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.StockQuoteSoapClient CL = new ServiceReference1.StockQuoteSoapClient();
+            Console.WriteLine(CL.GetQuote("BP"));
+            //
+            Console.ReadLine();
+
+        }
+    }
+}
